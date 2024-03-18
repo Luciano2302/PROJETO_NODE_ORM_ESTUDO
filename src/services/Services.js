b5e652a36f2a35aec316969c09d82184dfbf6c95
Luciano2302 <lucianolima.goncalves@gmail.com>
2024-03-18 10:58:30 -0300

    Implementação de Operdores e Agregadores do sequelize para os metodos do controller e modelos

--- src/services/Services.js
+++ src/services/Services.js
@@ -5,8 +5,8 @@ class Services {
     this.model = nomeDoModel;
   }
 
-  async pegaTodosOsRegistros() {
-    return dataSource[this.model].findAll();
+  async pegaTodosOsRegistros(where = {}) {
+    return dataSource[this.model].findAll({where : { ...where }});
   }
 
   async pegaRegistrosPorEscopo(escopo){
@@ -21,6 +21,9 @@ class Services {
     return dataSource[this.model].findOne({ where: { ...where }});
   }
 
+  async pegaEContaRegistros(options){
+    return dataSource[this.model].findAndCountAll({ ...options });
+  }
 
   async criaRegistro(dadosDoRegistro) {
     return dataSource[this.model].create(dadosDoRegistro);
